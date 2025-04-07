b36224d5339659ca031ffeab6e0d073d403cf025
William Dye <will2dye4@mac.com>
2025-04-07 15:07:57 -0400

    Add PID file to config

--- haikubot/config/gunicorn.conf.py
+++ haikubot/config/gunicorn.conf.py
@@ -9,6 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), 'config.json')) as f:
 
 bind = f'0.0.0.0:{CONFIG.get("server", {}).get("port", 5555)}'
 workers = multiprocessing.cpu_count() * 2
+pidfile = CONFIG.get('server', {}).get('pid_file_path')
 
 # SSL configuration
 certfile = CONFIG.get('ssl', {}).get('cert_path')
